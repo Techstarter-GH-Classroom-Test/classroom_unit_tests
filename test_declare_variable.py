b8b3beb2d35fc15f401b9d491b1bf5a2659d164a
HubertusTechstarter <152187829+HubertusTechstarter@users.noreply.github.com>
2024-08-14 12:27:06 +0200

    Update test_declare_variable.py

--- test_declare_variable.py
+++ test_declare_variable.py
@@ -1,11 +1,12 @@
 import unittest
+import main.py
 
 class TestNameVariable(unittest.TestCase):
     def test_name_variable(self):
         # Überprüfen, ob die Variable 'name' deklariert wurde
         self.assertTrue('name' in globals(), "Die Variable 'name' wurde nicht deklariert.")
         # Überprüfen, ob 'name' ein String ist
-        self.assertIsInstance(name, str, "Die Variable 'name' muss ein String sein.")
+        self.assertIsInstance(main.name, str, "Die Variable 'name' muss ein String sein.")
 
 if __name__ == '__main__':
     unittest.main()
